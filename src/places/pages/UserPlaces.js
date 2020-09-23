bce8a4e01ca5be963684b47084eb238b0eb8206d
karthikeyan sekar <karthikeyansep27@gmail.com>
2020-09-23 20:01:42 +1000

    adding google maps feature

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'one of the most famous sky scrapers in the world!',
+    imageUrl: 'https://cdn.getyourguide.com/img/location/5ca3484e4fa26.jpeg/148.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat:40.7484405,
+      lng: -73.9878584
+    },
+    creator: 'u1'
+  },
+  {
+    id: 'p1',
+    title: 'Sydney Harbour Bridge',
+    description: 'one of the most famous sky scrapers in the world!',
+    imageUrl: 'https://d12dkjq56sjcos.cloudfront.net/pub/media/magefan_blog/sightseeing/Sydney-Landmark-Sydney-Harbour-Bridge-Big-Bus-Tours.jpg',
+    address: '20 W 34th St, New York, NY 10001',
+    location: {
+      lat:-33.8548726,
+      lng: 151.2070922
+    },
+    creator: 'u2'
+  }
+
+];
+const UserPlaces = () => {
+  const userId = useParams().userId;
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
---
+++ src/shared/components/UIElements/Map.js
@@ -0,0 +1,29 @@
+import React, { useRef, useEffect } from 'react';
+
+import './Map.css';
+
+const Map = props => {
+  const mapRef = useRef();
+
+  const { center, zoom } = props;
+
+  useEffect(() => {
+    const map = new window.google.maps.Map(mapRef.current, {
+      center: center,
+      zoom: zoom
+    });
+
+    new window.google.maps.Marker({ position: center, map: map });
+  }, [center, zoom]);
+
+//whenever center and zoom changes useEffect gets triggered, here center and zoom are keys and values
+  return (
+    <div
+      ref={mapRef}
+      className={`map ${props.className}`}
+      style={props.style}
+    ></div>
+  );
+};
+
+export default Map;
